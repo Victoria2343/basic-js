0aff926f1d422c704d5859cbc8e737a6ca455502
Victoria2343 <vikusya234@mail.ru>
2020-11-01 23:13:13 +0300

    fix: change testing arg exist

--- src/what-season.js
+++ src/what-season.js
@@ -1,14 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if (date.length == 0) {
-     return 'Unable to determine the time of year!' 
+  if (!date) {
+     return 'Unable to determine the time of year!'; 
     };
   if (Object.prototype.toString.call(date) === '[object Date]') {
     const month = date.getMonth();
-    if (month.length == 0) {
-      return 'FAIL'; 
-    } else if (month === 0 || month === 1 || month === 11) {
+    if (month === 0 || month === 1 || month === 11) {
       return 'winter';
     } else if (month === 2 || month === 3 || month === 4) {
       return 'spring';
