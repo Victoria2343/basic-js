4644698aaad7f0b9a4b7878187ebd27e0bdd52f3
Victoria2343 <vikusya234@mail.ru>
2021-03-28 14:14:09 +0300

    fix: fix Carbon Dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,8 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  // if (sampleActivity = '') {
-  //   return false;
-  // }
-  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  if (typeof sampleActivity !== "string" || isNaN(+sampleActivity) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 };
