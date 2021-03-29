542f5d059aa9751083d3b549cc06760499f42f03
Victoria2343 <vikusya234@mail.ru>
2021-03-29 17:23:15 +0300

    feat: add to chainMaker getlength & addlink fn

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain:[],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push('( '+value+' )')
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   
   }
 };
 
