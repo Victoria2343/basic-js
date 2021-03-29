acea76193198001e8b5d0d29a320e97cbeecbef0
Victoria2343 <vikusya234@mail.ru>
2021-03-30 02:28:18 +0300

    feat: add encrypt fn to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  reverse = true;
+  constructor(reverse){
+    if(reverse == false){
+      this.reverse = false;
+    }
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Error');
+    let a = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+    
+    let arrMess = [...message.toUpperCase()];
+    let arrKey = [...key.toUpperCase()];
+    let pos = 0;
+
+    let res = arrMess.map(el => {
+      if (a.includes(el)) {
+        let posEl = (a.indexOf(arrKey[pos]) + a.indexOf(el)) % a.length;
+        pos = (pos + 1) % arrKey.length;
+        return a[posEl];
+      }
+      return el;
+    })
+    return this.reverse ? res.join('') : res.reverse().join('');
+
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+   
   }
 }
 
