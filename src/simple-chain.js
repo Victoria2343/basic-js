24328f85da6f6e164f99dff76491b332b589536d
Victoria2343 <vikusya234@mail.ru>
2021-03-29 18:11:17 +0300

    fix: fix simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -7,17 +7,30 @@ const chainMaker = {
     return this.chain.length;
   },
   addLink(value) {
-    this.chain.push('( '+value+' )')
+    if (typeof value !== 'undefined') {
+      this.chain.push(`( ${value} )`)
+    } else {
+      this.chain.push('( )');
+    }
     return this;
   },
   removeLink(position) {
-    
+    if (position > 0 && position < this.chain.length) {
+      this.chain.splice(position-1, 1);
+    } else {
+      this.chain = [];
+      throw new Error();
+    }
+    return this;
   },
   reverseChain() {
-   
+   this.chain.reverse();
+   return this;
   },
   finishChain() {
-   
+   let res = this.chain.join('~~');
+   this.chain.splice(0, this.getLength());
+   return res;
   }
 };
 
